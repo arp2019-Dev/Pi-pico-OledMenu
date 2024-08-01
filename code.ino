57cffd8972f8ee9204cfe69a099876771212b015
arp2019-Dev <130191124+arp2019-Dev@users.noreply.github.com>
2024-07-31 18:52:35 -0700

    Update code.ino

--- code.ino
+++ code.ino
@@ -4,6 +4,8 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <DHT.h>
+#include <IRremote.h>
+#include <TM1637Display.h>
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
@@ -18,25 +20,36 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define DHT_PIN 18
 #define DHT_TYPE DHT22
 
+#define IR_RECEIVE_PIN 19
+#define POT_PIN 26
+#define TM1637_CLK 20
+#define TM1637_DIO 21
+#define RELAY_PIN 22
+
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 DHT dht(DHT_PIN, DHT_TYPE);
+TM1637Display tm1637(TM1637_CLK, TM1637_DIO);
 
-const char* menu[] = {"Temperature", "Humidity", "Option 3", "Option 4", "Option 5"};
+const char* menu[] = {"Temperature", "Humidity", "IR Signal", "Potentiometer", "Option 5"};
 const int menuLength = sizeof(menu) / sizeof(menu[0]);
 int currentOption = 0;
 
 int lastClk = HIGH;
 bool inTemperatureMode = false;
 bool inHumidityMode = false;
+bool inIRMode = false;
+bool inPotentiometerMode = false;
 bool buttonPressed = false;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   
   pinMode(ENCODER_CLK, INPUT);
   pinMode(ENCODER_DT, INPUT);
   pinMode(ENCODER_SW, INPUT_PULLUP);
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW); // Ensure relay is off
 
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
@@ -51,6 +64,10 @@ void setup() {
   
   sensors.begin();
   dht.begin();
+  tm1637.setBrightness(0x0f); // Set maximum brightness
+
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+  
   displayMenu();
 }
 
@@ -66,7 +83,7 @@ void loop() {
       } else {
         currentOption = (currentOption - 1 + menuLength) % menuLength;
       }
-      if (!inTemperatureMode && !inHumidityMode) {
+      if (!inTemperatureMode && !inHumidityMode && !inIRMode && !inPotentiometerMode) {
         displayMenu();
       }
     }
@@ -74,7 +91,7 @@ void loop() {
 
   if (digitalRead(ENCODER_SW) == LOW) {
     if (!buttonPressed) {
-      delay(100);
+      delay(100); // Debounce delay
       buttonPressed = true;
       if (inTemperatureMode) {
         inTemperatureMode = false;
@@ -82,16 +99,28 @@ void loop() {
       } else if (inHumidityMode) {
         inHumidityMode = false;
         displayMenu();
-      } else if (currentOption == 0) { 
+      } else if (inIRMode) {
+        inIRMode = false;
+        displayMenu();
+      } else if (inPotentiometerMode) {
+        inPotentiometerMode = false;
+        displayMenu();
+      } else if (currentOption == 0) {
         inTemperatureMode = true;
         displayTemperature();
       } else if (currentOption == 1) {
         inHumidityMode = true;
         displayHumidity();
+      } else if (currentOption == 2) {
+        inIRMode = true;
+        displayIRSignal();
+      } else if (currentOption == 3) {
+        inPotentiometerMode = true;
+        displayPotentiometer();
       }
     }
   } else {
-    buttonPressed = false; 
+    buttonPressed = false;
   }
 }
 
@@ -126,7 +155,7 @@ void displayTemperature() {
     display.print(F(" C"));
     display.display();
 
-    delay(500); 
+    delay(500);
 
     if (digitalRead(ENCODER_SW) == LOW) {
       if (!buttonPressed) {
@@ -136,7 +165,7 @@ void displayTemperature() {
         displayMenu();
       }
     } else {
-      buttonPressed = false; 
+      buttonPressed = false;
     }
   }
 }
@@ -156,7 +185,7 @@ void displayHumidity() {
     display.print(F(" %"));
     display.display();
 
-    delay(500); 
+    delay(500);
 
     if (digitalRead(ENCODER_SW) == LOW) {
       if (!buttonPressed) {
@@ -166,7 +195,79 @@ void displayHumidity() {
         displayMenu();
       }
     } else {
-      buttonPressed = false; 
+      buttonPressed = false;
+    }
+  }
+}
+
+void displayIRSignal() {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println(F("IR Signal"));
+
+  while (inIRMode) {
+    if (IrReceiver.decode()) {
+      display.setCursor(0, 16);
+      display.setTextSize(2);
+      display.print(F("Signal: YES"));
+      display.display();
+      IrReceiver.resume();
+    } else {
+      display.setCursor(0, 16);
+      display.setTextSize(2);
+      display.print(F("Signal: NO "));
+      display.display();
+    }
+
+    delay(500);
+
+    if (digitalRead(ENCODER_SW) == LOW) {
+      if (!buttonPressed) {
+        delay(100); 
+        buttonPressed = true;
+        inIRMode = false;
+        displayMenu();
+      }
+    } else {
+      buttonPressed = false;
+    }
+  }
+}
+
+void displayPotentiometer() {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println(F("Potentiometer"));
+
+  while (inPotentiometerMode) {
+    int potValue = analogRead(POT_PIN);
+    int displayValue = map(potValue, 0, 4095, 0, 1000);
+
+    display.setCursor(0, 16);
+    display.setTextSize(2);
+    display.print(F("Value: "));
+    display.print(displayValue);
+    display.display();
+
+    tm1637.showNumberDec(displayValue, false);
+
+    if (displayValue == 1000) {
+      digitalWrite(RELAY_PIN, HIGH);
+    } else {
+      digitalWrite(RELAY_PIN, LOW);
+    }
+
+    delay(500);
+
+    if (digitalRead(ENCODER_SW) == LOW) {
+      if (!buttonPressed) {
+        delay(100); 
+        buttonPressed = true;
+        inPotentiometerMode = false;
+        displayMenu();
+      }
+    } else {
+      buttonPressed = false;
     }
   }
 }
